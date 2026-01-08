7e8abac69ab55d2d459db7ef9b6d880ab044ae4b
youcode safi <tajat.nourelhouda@gmail.com>
2026-01-08 14:43:18 +0100

    correction erreur bookRepository

--- src/Repositories/BookRepository.php
+++ src/Repositories/BookRepository.php
@@ -1,5 +1,7 @@
 <?php
 require_once '/../Core/Database.php';
+require_once '/../Entities/Book.php';
+require_once '/../Entities/Author.php';
 
 class BookRepository{
     private $db;
@@ -9,13 +11,30 @@ class BookRepository{
     }
 
     public function findAll(): array {
+        $books = [];
         $stmt = $this->db->prepare( 
             "SELECT b.title, b.price, b.stock, a.name FROM Book b
             INNER JOIN Author a ON a.id=b.author_id ORDER BY b.id"
         );
         $stmt->execute();
-        return $stmt->fetchAll();
+        $result= $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result as $res) {
+            $author = new Author(
+                $res['author_id'],
+                $res['name'],
+                $res['bio']
+            );
 
+            $books[] = new Book(
+                $res['id'],
+                $res['title'],
+                $author,
+                $res['price'],
+                $res['stock']
+            );
+        }
+
+        return $books; 
     }
     public function findByTitle($title): Book {
         $stmt = $this->db->prepare(
@@ -23,13 +42,14 @@ class BookRepository{
             INNER JOIN Author a ON a.id=b.author_id WHERE b.title= :title"
         );
         $stmt->execute(["title" => $title]);
-        return $stmt->fetch();
+        $stmt->fetch();
 
     }
     public function save(Book $book) : bool {
         $stmt = $this->db->prepare(
-            "INSERT INTO Book(title, price, stock, author_id) Values ()"
+           
         );
     }
 }
 ?>
+
